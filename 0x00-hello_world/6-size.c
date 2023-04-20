c7253b3797eb3e9c5c5833ff6f36bf7a4ef33b5f
LyneaW <wangecilynea@gmail.com>
2023-04-20 05:17:10 -0700

    Created 0x00-Hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-        print("Size of a char: %d byte(s)\n", sizeof(char));
-	print("Size of an int: %d byte(s)\n", sizeof(int));
-	print("Size of a long int: %d byte(s)\n", sizeof(long int));
-	print("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	print("Size of a float: %d byte(s)\n", sizeof(float));
-    	return (0);
+print("Size of a char: %d byte(s)\n", sizeof(char));
+print("Size of an int: %d byte(s)\n", sizeof(int));
+print("Size of a long int: %d byte(s)\n", sizeof(long int));
+print("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+print("Size of a float: %d byte(s)\n", sizeof(float));
+return (0);
 }
