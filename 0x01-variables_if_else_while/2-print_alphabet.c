9c784650f8bc068d0f81a49793be47944cf06d35
LyneaW <wangecilynea@gmail.com>
2023-04-24 03:34:23 -0700

    Created 0x01-Variable

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int n = 97;
+
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
