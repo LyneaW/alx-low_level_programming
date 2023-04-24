773fdbb3f478e7972df7de42797ca8305d20ac3b
LyneaW <wangecilynea@gmail.com>
2023-04-24 04:04:35 -0700

    Created 0x01-Variable

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int l;
+
+for (l = 0; l <= 9; l++)
+{
+putchar(l + 48);
+}
+putchar('\n');
+return (0);
+}
