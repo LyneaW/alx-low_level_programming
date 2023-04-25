271b07dbf8dcf2cf12d2b12c2c4f263a96a32c4b
LyneaW <wangecilynea@gmail.com>
2023-04-25 10:53:29 -0700

    Created 0x02-Functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,13 +19,9 @@ _putchar(',');
 _putchar(' ');
 t = l * w;
 if (t <= 99)
-{
 _putchar(' ');
-}
 if (t <= 9)
-{
 _putchar(' ');
-}
 if (t >= 100)
 {
 _putchar((t / 100) + '0');
@@ -34,10 +30,10 @@ _putchar(((t / 10) % 10) + '0');
 else if (t <= 99 && t >= 10)
 {
 _putchar((t / 10) + '0');
+}
 _putchar((t % 10) + '0');
 }
 _putchar('\n');
-}
 }
 }
 }
