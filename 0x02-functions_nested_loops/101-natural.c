c59f462d2d9fd6981ff42d88afe0b91de35c378b
LyneaW <wangecilynea@gmail.com>
2023-04-25 11:08:22 -0700

    Created 0x02-Functions

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of all the multiples of 3 or 5 below 1024
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int l;
+int w = 0;
 
+while (l < 1024)
+{
+if ((l % 3 == 0) || (l % 5 == 0))
+{
+w += l;
+}
+l++;
+}
+printf("%d\n", w);
+return (0);
+}
