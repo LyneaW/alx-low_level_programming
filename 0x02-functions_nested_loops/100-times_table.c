5e39f95cb81d5ce4e4ca2fae613e96919c025e23
LyneaW <wangecilynea@gmail.com>
2023-04-25 10:11:38 -0700

    Created 0x02-Functions

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_times_table - prints the n times table starting with 0
+ *
+ * @n: number of times table
+ */
+void print_times_table(int n)
+{
+int l, w, t;
+
+if (n >= 0 && n <= 15)
+{
+for (l = 0; l <= n; l++)
+{
+_putchar('0');
+for (w = 1; w <= n; w++)
+{
+_putchar(',');
+_putchar(' ');
+t = l * w;
+if (t <= 99)
+{
+_putchar(' ');
+}
+if (t <= 9)
+{
+_putchar(' ');
+}
+if (t >= 100)
+{
+_putchar((t / 100) + '0');
+_putchar(((t / 10) % 10) + '0');
+}
+else if (t <= 99 && t >= 10)
+{
+_putchar((t / 10) + '0');
+_putchar((t % 10) + '0');
+}
+_putchar('\n');
+}
+}
+}
+}
