3f2989c842e285c08fd367770b7d6257127fee60
LyneaW <wangecilynea@gmail.com>
2023-05-04 12:52:47 -0700

    Created 0x06-Pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,17 +4,24 @@
  * @dest: destination string pointer
  * @src: source string pointer
  * @n: number of bytes to be used
- * Return: destination string pointer
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int count_of_bytes;
+int m;
 
-for (count_of_bytes = 0; count_of_bytes < n && src[count_of_bytes] != '\0'; count_of_bytes++)
-dest[count_of_bytes] = src[count_of_bytes];
+m = 0;
+while (m < n && src[m] != '\0')
+{
+dest[m] = src[m];
+m++;
+}
+while (m < n)
+{
+dest[m] = '\0';
+m++;
+}
 
-for (; count_of_bytes < n; count_of_bytes++)
-dest[count_of_bytes] = '\0';
 return (dest);
 }
