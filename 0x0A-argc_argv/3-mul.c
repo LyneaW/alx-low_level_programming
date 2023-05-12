da5b3cca5fb28268ca07593c543e73db2c96bd99
LyneaW <wangecilynea@gmail.com>
2023-05-12 01:41:13 -0700

    Created 0x00-A

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of pointers
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+int n1 = 0, n2 = 0;
+
+if (argc == 3)
+{
+n1 = atoi(argv[1]);
+n2 = atoi(argv[2]);
+printf("%d\n", n1 *n2);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
+}
